61dedd0020944a24d6868394214ed58f4d8ce700
pedroppd <pedro_pdantas@hotmail.com>
2024-02-18 15:41:42 -0300

    Feat: Implementing flights controllers

---
+++ src/main/java/com/reactive/webfluxpatterns/sec02/controllers/FlightController.java
@@ -0,0 +1,28 @@
+package com.reactive.webfluxpatterns.sec02.controllers;
+
+import com.reactive.webfluxpatterns.sec02.dto.FlightResult;
+import com.reactive.webfluxpatterns.sec02.services.FlightSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping("sec02")
+public class FlightController {
+
+    @Autowired
+    private FlightSearchService service;
+
+    @GetMapping(value = "flights/{from}/{to}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<FlightResult> get(@PathVariable(name = "from") String from, @PathVariable(name = "to") String to) {
+        return service.search(from, to);
+    }
+
+
+}
