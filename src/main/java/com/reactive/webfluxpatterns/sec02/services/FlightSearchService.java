0638973884f303284613abac39921cbf0b261a91
pedroppd <pedro_pdantas@hotmail.com>
2024-02-18 15:44:15 -0300

    feat: Adding new url path

--- src/main/java/com/reactive/webfluxpatterns/sec02/services/FlightSearchService.java
+++ src/main/java/com/reactive/webfluxpatterns/sec02/services/FlightSearchService.java
@@ -19,12 +19,13 @@ public class FlightSearchService {
     @Autowired
     private Frontier frontier;
 
+    private static final Long DURATION = 3L;
+
     public Flux<FlightResult> search(String from, String to) {
         return Flux.merge(
                 jetBlue.get(new FlightDTO(from, to)),
                 delta.get(new FlightDTO(from, to)),
                 frontier.post(new FlightDTO(from, to))
-        )
-                .take(Duration.ofSeconds(3));
+        ).take(Duration.ofSeconds(DURATION));
     }
 }
