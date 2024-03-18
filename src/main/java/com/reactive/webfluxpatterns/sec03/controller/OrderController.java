824e1a3aa545a35b19a704902db80c0a5f818b65
pedroppd <pedro_pdantas@hotmail.com>
2024-03-18 16:35:42 -0300

    feat: creating OrderController

---
+++ src/main/java/com/reactive/webfluxpatterns/sec03/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.reactive.webfluxpatterns.sec03.controller;
+
+import com.reactive.webfluxpatterns.sec03.dto.OrderRequest;
+import com.reactive.webfluxpatterns.sec03.dto.OrderResponse;
+import com.reactive.webfluxpatterns.sec03.service.OrchestratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("sec03")
+public class OrderController {
+
+    @Autowired
+    private OrchestratorService service;
+
+    @PostMapping("order")
+    public Mono<ResponseEntity<OrderResponse>> placeOrder(@RequestBody Mono<OrderRequest> mono){
+        return this.service.placeOrder(mono)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+}
